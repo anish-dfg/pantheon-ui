ad0634d39e8d9004864bf4bde8ee8369b11276d4
jenpennen <jennycho35@gmail.com>
2024-08-22 21:08:56 -0700

    built job component

--- src/components/dashboard/jobs/Job.tsx
+++ src/components/dashboard/jobs/Job.tsx
@@ -1,4 +1,6 @@
+import { UpdateIcon } from "@radix-ui/react-icons";
 import { z } from "zod";
+import { Badge } from "~/components/ui/badge";
 
 export const AirtableImportBaseJobDataSchema = z.object({
   baseId: z.string(),
@@ -50,5 +52,70 @@ export const Job = ({
   description,
   details,
 }: JobProps) => {
-  return <div></div>;
+  const borderColor =
+    status === "complete"
+      ? "border-green-500"
+      : status === "pending"
+      ? "border-purple-500"
+      : "border-red-500";
+
+  return (
+    <div
+      className={`max-w-sm mx-auto bg-white shadow-md rounded-lg p-4 mt-6 border-2 ${borderColor}`}
+    >
+      <div className="mb-4">
+        {/* status */}
+        <span
+          className={`text-xs font-semibold inline-block py-1 px-2 rounded ${
+            status === "complete"
+              ? "bg-green-100 text-green-800"
+              : status === "pending"
+              ? "bg-purple-100 text-purple-800"
+              : status === "error"
+              ? "bg-red-100 text-red-800"
+              : "bg-red-100 text-red-800"
+          } `}
+        >
+          Status: {status}
+        </span>
+      </div>
+
+      {/* job id */}
+
+      <div className="text-lg font-semibold text-gray-800 mb-2">
+        Job ID: {id}
+      </div>
+
+      {/* project cycle id */}
+      {projectCycleId && (
+        <div className="text-md font-semibold text-gray-800 mb-2">
+          Project Cycle ID: {projectCycleId}
+        </div>
+      )}
+
+      {/* label */}
+      <div className="text-md font-medium text-gray-600 mb-2">{label}</div>
+
+      {/* created at and updated at */}
+      <div className="text-sm text-gray-500 mb-2">
+        <div>Created At: {new Date(createdAt).toLocaleDateString()}</div>
+        {updatedAt && (
+          <div>Updated At: {new Date(updatedAt).toLocaleDateString()}</div>
+        )}
+      </div>
+
+      {/* description */}
+      {description && (
+        <div className="text-gray-700 text-sm mb-2">
+          <p>{description}</p>
+        </div>
+      )}
+
+      {/* details */}
+
+      <div className="text-gray-700 text-sm">
+        <p>{details}</p>
+      </div>
+    </div>
+  );
 };
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,5 +1,6 @@
 // import { useAuth0 } from "@auth0/auth0-react";
 
+import { Job } from "~/components/dashboard/jobs/Job";
 import { LandingPageSkeleton } from "./LandingPageSkeleton";
 
 export const LandingPage = () => {
@@ -7,6 +8,22 @@ export const LandingPage = () => {
   return (
     <section className="min-h-screen bg-gray-100">
       <LandingPageSkeleton />
+      <Job
+        id="c3ad9a49-65f6-dw34-ds35-asdfgfdsasdfg"
+        projectCycleId="c3ad9a49-65f6-dw34-ds35-asdfgfdsasdfg"
+        status="complete"
+        label="ImportData"
+        createdAt="06-25-2024"
+        description="Insert Description"
+        details="Insert Detail"
+      />
+      <Job
+        id="c3ad9a49-65f6-dw34-ds35-asdfgfdsasdfg"
+        status="complete"
+        label="ImportData"
+        createdAt="06-21-2024"
+        details="Insert Detail"
+      />
     </section>
   );
 };
